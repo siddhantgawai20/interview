d4b4943b1d818f70a37eafa45e6e5736df6d2392
Siddhant Gawai <siddhantgawai20@gmail.com>
2023-10-03 19:20:22 +0530

    new

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Siddhant Gawai"
+    return "Siddhant Gawai is PRO"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
